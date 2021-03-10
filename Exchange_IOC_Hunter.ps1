f8c85cf02198a15eabc39138b4ed61402fbd2855
Dan Saunders <42096735+DCScoder@users.noreply.github.com>
2021-03-10 20:00:57 +0000

    Add files via upload

--- Exchange_IOC_Hunter.ps1
+++ Exchange_IOC_Hunter.ps1
@@ -1,7 +1,7 @@
 ###################################################################################
 #
 #    Script:    Exchange_IOC_Hunter.ps1
-#    Version:   1.1
+#    Version:   1.2
 #    Author:    Dan Saunders
 #    Contact:   dcscoder@gmail.com
 #    Purpose:   Hunt for IOCs in IIS Logs - CVE-2021-26855, CVE-2021-26857, CVE-2021-26858, CVE-2021-27065
@@ -24,7 +24,7 @@
 ###################################################################################
 
 $script = "Exchange_IOC_Hunter"
-$version = "v1.1"
+$version = "v1.2"
 
 # IOC
 # IP Address
@@ -61,10 +61,11 @@ $logs = Get-ChildItem -Recurse "$src\*.log"
 # Search Files
 
 # Remote Code Execution (RCE)
-Write-Host "`nHunting for potential malicious Remote Code Execution (RCE) (/ecp/DDI/DDIService.svc/SetObject)..." -ForegroundColor yellow -BackgroundColor black
+Write-Host "`nHunting for potential malicious Remote Code Execution (RCE)..." -ForegroundColor yellow -BackgroundColor black
 New-Item $dst\$dir\RCE -ItemType Directory | Out-Null
 New-Item $dst\$dir\RCE\Strings -ItemType Directory | Out-Null
 findstr /S /snip /c:"/ecp/DDI/DDIService.svc/SetObject" "$src\*.log" > $dst\$dir\RCE\Strings\DDIService_svc_SetObject.txt
+findstr /S /snip /c:"ResetOABVirtualDirectory" "$src\*.log" > $dst\$dir\RCE\Strings\ResetOABVirtualDirectory.txt
 
 # JavaScript
 Write-Host "`nHunting for potential malicious Javascript (.js) files..." -ForegroundColor yellow -BackgroundColor black
@@ -88,6 +89,33 @@ findstr /S /snip /c:"/Error.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\Error.
 findstr /S /snip /c:"/Logout.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\Logout.aspx.txt
 findstr /S /snip /c:"/help.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\Help.aspx.txt
 findstr /S /snip /c:"/iisstart.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\iisstart.aspx.txt
+findstr /S /snip /c:"/Server.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\Server.aspx.txt
+findstr /S /snip /c:"/Supp0rt.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\Supp0rt.aspx.txt
+findstr /S /snip /c:"/xx.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\xx.aspx.txt
+findstr /S /snip /c:"/xclkmcfldfi948398430fdjkfdkj.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\xclkmcfldfi948398430fdjkfdkj.aspx.txt
+findstr /S /snip /c:"/log.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\log.aspx.txt
+findstr /S /snip /c:"/iispage.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\iispage.aspx.txt
+findstr /S /snip /c:"/s.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\s.aspx.txt
+findstr /S /snip /c:"/a.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\a.aspx.txt
+findstr /S /snip /c:"/shell2.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\shell2.aspx.txt
+findstr /S /snip /c:"/shell90.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\shell90.aspx.txt
+findstr /S /snip /c:"/default1.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\default1.aspx.txt
+findstr /S /snip /c:"/default.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\default.aspx.txt
+findstr /S /snip /c:"/one.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\one.aspx.txt
+findstr /S /snip /c:"/one1.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\one1.aspx.txt
+findstr /S /snip /c:"/log.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\log.aspx.txt
+findstr /S /snip /c:"/logg.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\logg.aspx.txt
+findstr /S /snip /c:"/bob.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\bob.aspx.txt
+findstr /S /snip /c:"/OutlookZH.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\OutlookZH.aspx.txt
+findstr /S /snip /c:"/w7tAhF9i1pJnRo.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\w7tAhF9i1pJnRo.aspx.txt
+findstr /S /snip /c:"/authhead.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\authhead.aspx.txt
+findstr /S /snip /c:"/fatal-erro.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\fatal-erro.aspx.txt
+findstr /S /snip /c:"/errorPage.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\errorPage.aspx.txt
+findstr /S /snip /c:"/errorPages.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\errorPages.aspx.txt
+findstr /S /snip /c:"/aspnet_client.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\aspnet_client.aspx.txt
+findstr /S /snip /c:"/aspnet_iisstart.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\aspnet_iisstart.aspx.txt
+findstr /S /snip /c:"/aspnet_pages.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\aspnet_pages.aspx.txt
+findstr /S /snip /c:"/aspnet_www.aspx" "$src\*.log" > $dst\$dir\Files\WebShell\aspnet_www.aspx.txt
 
 # HTTP POST
 Write-Host "`nHunting for potential malicious HTTP POST requests (themes file path only)..." -ForegroundColor yellow -BackgroundColor black
